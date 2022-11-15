edd276d87c2f14fe12159c92061572785d1c4ff4
Ilona Shishov <Ilona.Shishov@gmail.com>
2022-11-15 16:53:14 +0200

    feat: Added login to k8s cluster (#20)
    * feat: added login to API server feature
    
    Signed-off-by: Ilona Shishov <Ilona.Shishov@gmail.com>
    
    * chore: re-phrase annotations and remove un-used arguments
    
    Signed-off-by: Ilona Shishov <Ilona.Shishov@gmail.com>
    
    * feat: added logout from API server feature
    
    Signed-off-by: Ilona Shishov <Ilona.Shishov@gmail.com>
    
    * chore: added login state view refresh
    
    Signed-off-by: Ilona Shishov <Ilona.Shishov@gmail.com>
    
    * chore: added input validation
    
    Signed-off-by: Ilona Shishov <Ilona.Shishov@gmail.com>
    
    * fix: for missing declaration file for module 'validator'
    
    Signed-off-by: Ilona Shishov <Ilona.Shishov@gmail.com>
    
    * fix: restrict template expressions alert
    
    Signed-off-by: Ilona Shishov <Ilona.Shishov@gmail.com>
    
    * fix: syntax warnings
    
    Signed-off-by: Ilona Shishov <Ilona.Shishov@gmail.com>
    
    Signed-off-by: Ilona Shishov <Ilona.Shishov@gmail.com>

---
+++ src/commands/logout.ts
@@ -0,0 +1,21 @@
+import * as connect from '../utils/connectToServer';
+import * as vscode from 'vscode';
+import { login } from './login';
+
+export async function logout(): Promise<void> {
+
+	const action = await vscode.window.showWarningMessage('Do you want to logout of cluster?', 'Logout', 'Cancel');
+	if (action === 'Logout') {
+		await connect.disconnect()
+		.catch((err: string | Error) =>
+			vscode.window.showErrorMessage(err instanceof Error ? err.name : err))
+		.then(async (msg) => {
+				await vscode.commands.executeCommand('setContext', 'isLoggedIn', false);
+				const logoutInfo = await vscode.window.showInformationMessage(`${msg || 'Process outcome undefined'}. Do you want to login to a new cluster?`, 'Yes', 'No');
+				if (logoutInfo === 'Yes') {
+					return login();
+				}
+				return;
+		});
+	}
+}
